d4b4324e3a7003eb46c499bad87e5f52c7042cd1
ivan ramos <ivan.ramos@rover.com>
2020-04-13 17:04:42 -0500

    Menu with 2 options: Feeds and settings

--- app/src/main/java/edu/expandedmind/keepmeuptodate/FeedsActivity.kt
+++ app/src/main/java/edu/expandedmind/keepmeuptodate/FeedsActivity.kt
@@ -36,7 +36,7 @@ class FeedsActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
