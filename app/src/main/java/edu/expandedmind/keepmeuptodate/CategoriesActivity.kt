2152284fea029836585a4e190b32a64ac6592345
ivan ramos <ivan.ramos@rover.com>
2020-04-13 16:26:15 -0500

    Add couple of TODOs

--- app/src/main/java/edu/expandedmind/keepmeuptodate/CategoriesActivity.kt
+++ app/src/main/java/edu/expandedmind/keepmeuptodate/CategoriesActivity.kt
@@ -2,11 +2,22 @@ package edu.expandedmind.keepmeuptodate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_categories.*
 
 class CategoriesActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
+        setupUIListeners()
     }
+
+    private fun setupUIListeners() {
+        downloadBtn.setOnClickListener {view ->
+            //TODO add code here to open up FeedsActivity
+        }
+    }
+
+
 }
--- app/src/main/java/edu/expandedmind/keepmeuptodate/FeedsActivity.kt
+++ app/src/main/java/edu/expandedmind/keepmeuptodate/FeedsActivity.kt
@@ -14,7 +14,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
-class FeedActivity : AppCompatActivity() {
+class FeedsActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
